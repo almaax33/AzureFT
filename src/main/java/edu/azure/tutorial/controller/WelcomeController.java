766bae6b63c9dae4d417280b269ce4b07fb38544
Alma Garcia <alma.garcia@kroger.com>
2021-01-31 17:28:07 -0500

    Alma Garcia | Add stage environment

---
+++ src/main/java/edu/azure/tutorial/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package edu.azure.tutorial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Welcome :) ";
+    }
+}
